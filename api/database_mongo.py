09470a40eaf26488760ab57d7773877b4ff77aec
ALEJANDRO <ALEJANDRO@DESKTOP-8QQCSOH>
2020-02-15 17:30:20 +0100

    Update database_mongo.py

--- api/database_mongo.py
+++ api/database_mongo.py
@@ -77,5 +77,5 @@ def update_product(product: dict):
     new_product = product.copy()
     new_product.pop("id")
 
-    collection.update_one({"_id": ObjectId(product["id"])}, {"$set": new_product})
+    collection.update_one({"_id": ObjectId(product["id"])}, new_product)
 
