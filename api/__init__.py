37a40e90d165b5f1695b2b00802c443761904c0e
ALEJANDRO <ALEJANDRO@DESKTOP-8QQCSOH>
2020-02-22 19:16:45 +0100

    Added Websockets

--- api/__init__.py
+++ api/__init__.py
@@ -1,7 +1,10 @@
 
 from flask import Flask
+from flask_socketio import SocketIO
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "OfUJnnclWc7iAWap1qsr"
+socketio = SocketIO(app)
 
 import api.service
+import api.socket_service
